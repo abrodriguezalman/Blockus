133bb3c246943560e9cedde56abbcc7bc3981650
Abril Rodriguez <abrilrodriguez@Abrils-MacBook-Air.local>
2024-05-02 10:38:25 -0500

    Finalized tui for Milestone #1

--- src/tui.py
+++ src/tui.py
@@ -1,26 +1,36 @@
 import sys
 from fakes import BlokusStub
 
-size = int(sys.argv[1])
-start_positions = {(int(size/2 - 1), int(size/2 - 1)), (int(size/2),int(size/2))}
-game = BlokusStub(2, size, start_positions)
+red: str = "\x1b[1;31m"
+blue: str = "\x1b[1;34m"
+black: str = "\x1b[1;30m"
 
-print("┌──┐" * size)
+size: int = int(sys.argv[1])
+start_positions: set[tuple[int, int]]= {(size//2 - 1, size//2 - 1), (size//2, size//2)}
+game: 'BlokusStub' = BlokusStub(2, size, start_positions)
 
-for i, r in enumerate(game.grid):
-    row: list[str] = []
-    for j, c in enumerate(r):
-        if c is None:
-            if (i, j) in start_positions:
-                row.append("│\x1b[1;30m██\x1b[1;30m│")
+def draw_board(game: 'BlokusStub') -> str:
+    print("┌" + "──┬" * game.size)
+
+    for i, row in enumerate(game.grid):
+        board_row: list[str] = []
+        
+        for j, cell in enumerate(row):
+            if cell is None:
+                if (i, j) in start_positions:
+                    board_row.append("██" + black)
+                else:
+                    board_row.append("  ")
+            elif cell[0] == 1:
+                board_row.append(blue + "██" + black)
             else:
-                row.append("│  │")
-        elif c[0] == 1:
-            row.append("│\x1b[1;34m██\x1b[1;30m│")
-        else:
-            row.append("│\x1b[1;31m██\x1b[1;30m│")
-    print(''.join(row))
-    if i != size - 1:
-        print("├──┤" * size)
+                board_row.append(red + "██" + black)
+        
+        print("│" + '│'.join(board_row)+"│")
+        
+        if i != size - 1:
+            print("├──" * size + "┤")
+    
+    print("└" + "──┴" * game.size)
 
-print("└──┘" * size)
+draw_board(game)
