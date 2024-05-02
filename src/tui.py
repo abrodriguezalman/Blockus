3944034652cb970a0960663c0f1f7cba5eee5e28
Abril Rodriguez <abrilrodriguez@Abrils-MacBook-Air.local>
2024-05-02 10:52:03 -0500

    Finalized tui for Milestone #1

--- src/tui.py
+++ src/tui.py
@@ -5,16 +5,24 @@ red: str = "\x1b[1;31m"
 blue: str = "\x1b[1;34m"
 black: str = "\x1b[1;30m"
 
-size: int = int(sys.argv[1])
-start_positions: set[tuple[int, int]]= {(size//2 - 1, size//2 - 1), (size//2, size//2)}
-game: 'BlokusStub' = BlokusStub(2, size, start_positions)
+sz: int = int(sys.argv[1])
+start_positions: set[tuple[int, int]]= {(sz//2 - 1, sz//2 - 1), (sz//2, sz//2)}
+stub_game: 'BlokusStub' = BlokusStub(2, sz, start_positions)
 
 def draw_board(game: 'BlokusStub') -> str:
+    """
+    Draws the board for a given BlokusStub game object.
+
+    Inputs:
+        game [BlokusStub]: a Blokus game object
+    
+    Returns [str]: a string representation of the game board.
+    """
     print("┌" + "──┬" * game.size)
 
     for i, row in enumerate(game.grid):
         board_row: list[str] = []
-        
+
         for j, cell in enumerate(row):
             if cell is None:
                 if (i, j) in start_positions:
@@ -25,12 +33,12 @@ def draw_board(game: 'BlokusStub') -> str:
                 board_row.append(blue + "██" + black)
             else:
                 board_row.append(red + "██" + black)
-        
+
         print("│" + '│'.join(board_row)+"│")
-        
-        if i != size - 1:
-            print("├──" * size + "┤")
-    
+
+        if i != game.size - 1:
+            print("├──" * game.size + "┤")
+
     print("└" + "──┴" * game.size)
 
-draw_board(game)
+draw_board(stub_game)
